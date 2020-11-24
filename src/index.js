d104ccd26931e96f96e0694e7be813cb5dd0db1d
StevenWuTG <stevenwu91@icloud.com>
2020-11-23 22:15:27 -0500

    donezo

--- src/index.js
+++ src/index.js
@@ -2,22 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const url = "http://localhost:3000/dogs"
 
     const tableBody = document.querySelector("#table-body")
+    const dogForm = document.querySelector("#dog-form")
+
+    // function resetDogForm(event){
+    //     event.preventDefault()
+    //     dogForm.name.value = ""
+    //     dogForm.breed.value = ""
+    //     dogForm.sex.value = ""
+        
+    // }
 
 
+    // maybe function go here
+    function renderAllDogs(){
+        tableBody.innerHTML = ''
+        fetch(url)
+        .then(r => r.json())
+        .then(dogArr => {
+            dogArr.forEach(dog => {
+    
+                renderDog(dog)
+            });
+        })
+
+    }
+
+    renderAllDogs()
 
 
-
-
-
-
-    fetch(url)
-    .then(r => r.json())
-    .then(dogArr => {
-        dogArr.forEach(dog => {
-
-            renderDog(dog)
-        });
-    })
 
     function renderDog(dog){
 
@@ -29,6 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const dogSex = document.createElement("td")
         const editDogBtn = document.createElement("button")
         editDogBtn.textContent = "Edit Doggo"
+        editDogBtn.id = dog.id
 
         dogName.textContent = dog.name
         dogBreed.textContent = dog.breed
@@ -38,8 +51,115 @@ document.addEventListener('DOMContentLoaded', () => {
 
         tableBody.append(dogTr)
 
+
+
+        editDogBtn.addEventListener("click", function(e){
+
+            dogForm.name.value = dog.name
+            dogForm.breed.value = dog.breed
+            dogForm.sex.value = dog.sex
+            
+            editDog(dog)
+        })
+        
+        function editDog(dog){
+            
+            const id = dog.id
+            
+            
+            submitBtn = document.querySelector("#dog-submit")
+            // debugger
+            
+            dogForm.addEventListener("click", function(e){
+                // debugger
+                if(e.target.tagName === "INPUT"){
+                    e.preventDefault()
+                    
+                    const dogObj = {
+                        name: dogForm.name.value,
+                        breed: dogForm.breed.value,
+                        sex: dogForm.sex.value
+                    }
+                    // debugger
+                    fetch(`${url}/${id}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(dogObj),
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                        console.log('Success:', data);
+                         renderAllDogs(data)
+                        //  resetDogForm()
+                        
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+                    // debugger
+                   
+                    
+                    // e.target.reset()
+                    
+                }
+                
+            })
+
+            // submitBtn.addEventListener("click", function(e){
+                
+            // })
+            
+            
+                
+        }
+
+    
+        
     }
 
+
+
+
+
+
+
+
+
+
+    // icebox 
+
+    // editDogBtn.addEventListener("click", editDog)
+
+    // function editDog(event){
+    //     // debugger
+    //     if(event.target.tagName === 'BUTTON'){
+    //         const id = event.target.id
+    //         console.log(id)
+
+            // fetch(`${url}/${id}`, {
+            // method: 'PATCH', // or 'PUT'
+            // headers: {
+            //     'Content-Type': 'application/json',
+            // },
+            // body: JSON.stringify(data),
+            // })
+            // .then(response => response.json())
+            // .then(data => {
+            // console.log('Success:', data);
+            // })
+            // .catch((error) => {
+            // console.error('Error:', error);
+            // });
+    //     }
+
+    //     dogForm.name.value = dog.name
+    //     dogForm.breed.value = dog.breed
+    //     dogForm.sex.value = dog.sex
+
+    // }
+    
 
 
 
